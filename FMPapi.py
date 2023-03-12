1d78674f4218480f75297bc5f7b94bff2e1f5251
bnebe <bnebeker95@gmail.com>
2023-03-11 22:39:13 -0700

    updated to dos

--- FMPapi.py
+++ FMPapi.py
@@ -22,7 +22,7 @@ def get_symbols():
     symbol_url = f"https://financialmodelingprep.com/api/v3/stock/list?apikey={api_key}"
     symbol_data = get_api_data(symbol_url)
     symbol_df = pd.read_json(symbol_data)
-    symbol_df = symbol_df[symbol_df['type'] == 'stock']
+    symbol_df = symbol_df[symbol_df['type'] == 'stock'].copy()
     #symbols = list(symbol_df['symbol'].drop_duplicates())
     return symbol_df
 
@@ -54,9 +54,10 @@ def get_price_data(symbol):
 
     # Lag and lead variables
     columns_to_shift = ['open', 'high', 'low', 'close', 'adjClose', 'volume', 'unadjustedVolume']
-    lags = [1, -1, -30, -90, -180, -360]
+    lags = [360, 180, 90, 30, 1, -1, -30, -90, -180, -360]
     for lag in lags:
         for column in columns_to_shift:
+            # TODO: Remove negatives from names, if statement to state lag or lead
             price_df[str(lag) + '_lag_' + str(column)] = price_df[column].shift(lag)
     return price_df
 
@@ -77,22 +78,23 @@ def combine_earnings_price(symbol, price_df):
 
         # Create and adjust dataframe for earnings data
         earn_data = get_api_data(earnings_url)
-        if earn_data == '[]':
-            if previous_blank:
-                if all_earnings_df.empty:
+        if earn_data == '[]':  # Check for empty api call
+            if previous_blank:  # Check if 2 empty api calls happened back to back
+                if all_earnings_df.empty:  # If no api call succeeded return nothing
                     return
                 else:
-                    try:
+                    try:  # Try to merge earnings and price
                         result = pd.merge(all_earnings_df, price_df, on='date', how='inner')
                         return result
-                    except KeyError:
-                        return
+                    except KeyError:  # Sometimes data miss match, if so return an empty df
+                        result = pd.DataFrame()
+                        return result
             previous_blank = True
             continue
         earn_df = pd.read_json(earn_data)
         earn_df['time'] = pd.to_datetime(earn_df['date']).dt.time
-        earn_df['date'] = pd.to_datetime(earn_df['date']).dt.date
-        earn_df['date'] = pd.to_datetime(earn_df['date'])
+        earn_df['date'] = pd.to_datetime(earn_df['date'].dt.date)
+        # earn_df['date'] = pd.to_datetime(earn_df['date'])
         all_earnings_df = pd.concat([all_earnings_df, earn_df], ignore_index=True)
         previous_blank = False
 
@@ -101,14 +103,22 @@ def combine_earnings_price(symbol, price_df):
 
 
 symbol_df = get_symbols()
-symbol_df = symbol_df.loc[symbol_df['exchangeShortName'] == 'NYSE']
-# symbol_df = symbol_df.loc[(symbol_df['exchangeShortName'] == 'NYSE') | (symbol_df['exchangeShortName'] == 'NASDAQ')]
-symbol_df = symbol_df[symbol_df['price']>200]
+symbol_df = symbol_df.loc[(symbol_df['exchangeShortName'] == 'NYSE') | (symbol_df['exchangeShortName'] == 'NASDAQ')]
+symbol_df = symbol_df[symbol_df['price']>0]
 symbols = list(symbol_df['symbol'].drop_duplicates())
-data = pd.DataFrame()
+data = []  # Switched to list of dataframes for faster run time
 for symbol in tqdm(symbols, desc="Collecting data"):
     price_df = get_price_data(symbol)
     result = combine_earnings_price(symbol, price_df)
-    data = pd.concat([data, result], ignore_index=True)
+    # TODO: Clean up df to remove same names before merge
+    data.append(result)
+    #data = pd.concat([data, result], ignore_index=True)
 
-data.to_parquet('nyse_price_100.prq')
+# concats all dfs
+df = pd.concat(data, ignore_index=True)
+data.to_parquet('initial_data.prq')
+
+
+# TODO: engineer features to act as inputs or targets, % change as it relates to broader market
+# TODO: look at how a stock price changes vs VTI, if it increases 10% but the rest of the market avg'ed at +20% its not as good
+# TODO: categorize stocks, could be useful for forced diversification
